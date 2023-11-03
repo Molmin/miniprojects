3484a102a8179bace8c6ef04c82eb8f734bc89af
Molmin <115549630@qq.com>
2023-11-03 10:29:06 +0800

    feat(hydrooj-problem-download): add upload support

--- hydrooj-problem-download/index.ts
+++ hydrooj-problem-download/index.ts
@@ -1,10 +1,9 @@
-import { createWriteStream, readFileSync, writeFileSync } from 'fs'
+import { createWriteStream, readFile, readFileSync, readdirSync, writeFileSync } from 'fs'
 import { ensureDirSync } from 'fs-extra'
 import superagent from 'superagent'
 import yamljs from 'yamljs'
 
 class Service {
-    public logs: string[] = [];
     constructor(
         public endPoint: string,
         public cookie: string,
@@ -27,12 +26,22 @@ class Service {
             .set('Cookie', this.cookie)
             .accept('application/json')
     }
-    log(content: string) { this.logs.push(content) }
 
     async getLoggedInUser(): Promise<string> {
         const { body: { UserContext } } = await this.get(`/`)
         const json = JSON.parse(UserContext)
         return json?.uname
+    }
+
+    existsProblem(pid: string): Promise<boolean> {
+        return new Promise((resolve, reject) =>
+            this.get(`/p/${pid}`)
+                .end((error, { status }) => {
+                    if (status === 200) resolve(true)
+                    else if (status === 404) resolve(false)
+                    else reject()
+                })
+        )
     }
 
     async getProblemSummary(pid: string, target: string) {
@@ -53,6 +62,20 @@ class Service {
         console.log(`Saved Problem Summary`)
     }
 
+    async createProblem(pid: string, path: string) {
+        const content = readFileSync(`${path}/problem_zh.md`).toString()
+        const { title, tag } = yamljs.load(`${path}/problem.yaml`)
+        const response = await this.post('/problem/create')
+            .send({
+                pid: /^[a-zA-Z]+[a-zA-Z0-9]*$/i.test(pid) ? pid : '',
+                tag: tag.join(','),
+                difficulty: '',
+                title, content,
+            })
+        console.log(`Created problem ${this.domainId}/${pid}`)
+        return response.body.pid
+    }
+
     async getFiles(pid: string) {
         const { body } = await this.get(`/p/${pid}/files`)
         return {
@@ -60,7 +83,6 @@ class Service {
             additional_file: body.additional_file.map((file: { name: string }) => file?.name),
         }
     }
-
     async getLinks(pid: string, files: string[], type: 'testdata' | 'additional_file') {
         const links = await this.post(`/p/${pid}/files`)
             .send({ operation: "get_links", type, files })
@@ -73,17 +95,22 @@ class Service {
         const stream = createWriteStream(`data/${target}`)
         request.pipe(stream)
         await new Promise((resolve, reject) => {
-            stream.on('finish', resolve);
-            stream.on('error', reject);
-            request.on('error', reject);
-            request.on('timeout', reject);
-        });
+            stream.on('finish', resolve)
+            stream.on('error', reject)
+            request.on('error', reject)
+            request.on('timeout', reject)
+        })
         console.log(`Downloaded File ${target}`)
     }
 
-    async getProblemList(page: number) {
-        const list = await this.get('/p').query({ page })
-        return list.body.pdocs
+    async uploadFile(pid: string, type: 'testdata' | 'additional_file', filename: string, path: string) {
+        await this.post(`/p/${pid}/files`)
+            .field({
+                filename, type,
+                operation: 'upload_file',
+            })
+            .attach('file', readFileSync(path))
+        return await this.getFiles(pid)
     }
 }
 
@@ -91,11 +118,15 @@ interface DownloadConfig {
     oj_url: string
     cookie_sid: string
     domain: string
-    problem: {
+    download: {
         pid: string
         additional_file: boolean
         testdata: boolean
         statement: boolean
+    }[]
+    upload: {
+        pid: string
+        path: string
     }[]
 }
 
@@ -111,7 +142,7 @@ async function main() {
     const username = await service.getLoggedInUser()
     if (username === 'Guest') return console.error(`Not logged in`)
     console.log(`Logged in ${username}`)
-    for (let problem of config.problem) {
+    for (let problem of config.download) {
         const { pid } = problem
         console.log(`Downloading problem ${config.domain}/${pid}`)
         const url_prefix = `${service.domainId}/${pid}`
@@ -128,6 +159,30 @@ async function main() {
             const additional_file_links = await service.getLinks(pid, additional_file, 'additional_file')
             for (let [filename, link] of Object.entries(additional_file_links))
                 await service.downloadFile(link as string, `${url_prefix}/additional_file/${filename}`)
+        }
+    }
+    for (let problem of config.upload) {
+        const { path } = problem
+        let pid = problem.pid
+        console.log(`Uploading problem ${config.domain}/${pid}`)
+        if (!await service.existsProblem(pid))
+            pid = await service.createProblem(pid, path)
+        const testdata = readdirSync(`${path}/testdata`)
+        const additional_file = readdirSync(`${path}/additional_file`)
+        let files = await service.getFiles(pid)
+        for (let file of testdata) {
+            if (files.testdata.includes(file)) continue
+            files = await service.uploadFile(pid, 'testdata', file, `${path}/testdata/${file}`)
+            if (files.testdata.includes(file))
+                console.log(`Successfully uploaded file ${file}`)
+            else console.log(`Failed to upload file ${file}`)
+        }
+        for (let file of additional_file) {
+            if (files.additional_file.includes(file)) continue
+            files = await service.uploadFile(pid, 'additional_file', file, `${path}/additional_file/${file}`)
+            if (files.additional_file.includes(file))
+                console.log(`Successfully uploaded file ${file}`)
+            else console.log(`Failed to upload file ${file}`)
         }
     }
 }
