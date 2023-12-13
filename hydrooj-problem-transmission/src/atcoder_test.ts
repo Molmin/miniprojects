be7868ab56fc5870163b22be128689751beac663
Molmin <115549630@qq.com>
2023-12-13 17:36:19 +0800

    feat(transmission): add atcoder_test

---
+++ hydrooj-problem-transmission/src/atcoder_test.ts
@@ -0,0 +1,20 @@
+import HydroAccountService from './basic/service'
+
+const service = new HydroAccountService(
+    'https://hydro.ac', '', 'atcoder',
+)
+
+async function main() {
+    let countPages = 1, list: string[] = []
+    for (let page = 1; page <= countPages; page++) {
+        console.log(`Getting page ${page} / ${countPages}`)
+        let { body: { ppcount, pdocs } } = await service.get('/p').query({ page })
+        countPages = ppcount
+        pdocs = pdocs.filter((pdoc: any) => pdoc.nSubmit > 0 && pdoc.nAccept === 0)
+        pdocs = pdocs.map((pdoc: { pid: string }) => pdoc.pid) as string[]
+        list = list.concat(pdocs)
+    }
+    console.log(list)
+}
+
+main()
